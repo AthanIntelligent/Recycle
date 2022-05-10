0b0a62ef32165b6e82b4f8b18c534a3e2194b602
xingshuang <xingshuang@css.com.cn>
2022-05-10 17:49:53 +0800

    10.7

--- src/main/java/com/jack/recycle/controller/TransactionController.java
+++ src/main/java/com/jack/recycle/controller/TransactionController.java
@@ -1,15 +1,14 @@
 package com.jack.recycle.controller;
 
+import com.jack.recycle.model.PO.TransactionGoods;
+import com.jack.recycle.model.Reservation;
 import com.jack.recycle.model.Transaction;
-import com.jack.recycle.model.TransactionGoods;
-import com.jack.recycle.model.VO.StationAndUser;
 import com.jack.recycle.model.VO.TransactionAndGoods;
 import com.jack.recycle.service.*;
 import com.jack.recycle.utils.DateUtils;
 import com.jack.recycle.utils.Result;
 import com.jack.recycle.utils.StatusCode;
 import com.jack.recycle.utils.UserUtils;
-import com.sun.org.apache.bcel.internal.generic.NEW;
 import org.hibernate.validator.internal.util.StringHelper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.util.StringUtils;
@@ -36,30 +35,35 @@ public class TransactionController {
     @Autowired
     private TransactionGoodsService transactionGoodsService;
 
-    @Autowired
-    private StationService stationService;
-
     @PostMapping(value = "/addTransaction")
     public Result addTransaction(@RequestBody TransactionAndGoods transactionAndGoods) {
-        Transaction transaction = transactionAndGoods.getTransaction();
+        Reservation reservation = transactionAndGoods.getReservation();
+        Transaction transaction = new Transaction();
         transaction.setUuid(UUID.randomUUID().toString());
         //传用户真实姓名过来转成uuid
-        String userId = userService.getUuidByRealName(transaction.getUserId());
+
+        String userId = userService.getUuidByRealName(reservation.getAppointmentHolder());
         transaction.setUserId(userId);
-        //添加交易记录时，基站名称和法人姓名 自动显示且按钮禁用，点击保存时，传id，单独写一个接口根据当前登录人id返回人员和基站
-//        transaction.setStationLegal(UserUtils.getCurrUserInfo().getUuid());
-        String formatDate = DateUtils.getFormatDate("yyyy-MM-dd HH:mm");
-        transaction.setTransactionTime(formatDate);
+        transaction.setStationId(reservation.getAppointmentStation());
+        transaction.setTransactionTime(DateUtils.getFormatDate("yyyy-MM-dd HH:mm"));
+        transaction.setStationLegal(UserUtils.getCurrUserInfo().getUuid());
+        transaction.setAllMoney(transactionAndGoods.getAllMoney());
+        transaction.setPayFlag(transactionAndGoods.getPayFlag());
 
         List<String> goodsIdsList = new ArrayList<>();
         //获取交易物品list
-        List<TransactionGoods> transactionGoodsList = transactionAndGoods.getTransactionGoodsList();
-        for (TransactionGoods tran : transactionGoodsList) {
-            tran.setUuid(UUID.randomUUID().toString());
-            tran.setTransactionId(transaction.getUuid());
-            transactionGoodsService.addTransactionGoods(tran);
-            goodsIdsList.add(tran.getUuid());
+        List<com.jack.recycle.model.PO.TransactionGoods> transactionGoodsList = transactionAndGoods.getTransactionGoodsList();
+        for (com.jack.recycle.model.PO.TransactionGoods tr: transactionGoodsList) {
+            com.jack.recycle.model.TransactionGoods transactionGoods = new com.jack.recycle.model.TransactionGoods();
+            transactionGoods.setUuid(UUID.randomUUID().toString());
+            transactionGoods.setTransactionId(transaction.getUuid());
+            transactionGoods.setGoodsId(tr.getUuid());
+            transactionGoods.setWeight(tr.getGoodsWeight());
+            transactionGoods.setMoney(tr.getGoodsPrice());
+            transactionGoodsService.addTransactionGoods(transactionGoods);
+            goodsIdsList.add(transactionGoods.getUuid());
         }
+
         String goodsIds = String.join(",", goodsIdsList);
         transaction.setTransactionGoodsId(goodsIds);
         transactionService.addTransaction(transaction);
@@ -94,10 +98,10 @@ public class TransactionController {
 
     @GetMapping(value = "/getTransactionGoods")
     public Result getTransactionGoods(@RequestParam("transactionId") String transactionId) {
-        TransactionGoods transactionGoods = new TransactionGoods();
+        com.jack.recycle.model.TransactionGoods transactionGoods = new com.jack.recycle.model.TransactionGoods();
         transactionGoods.setTransactionId(transactionId);
-        List<TransactionGoods> transactionGoodsList = transactionGoodsService.dirTransactionGoods(transactionGoods);
-        for (TransactionGoods tr : transactionGoodsList) {
+        List<com.jack.recycle.model.TransactionGoods> transactionGoodsList = transactionGoodsService.dirTransactionGoods(transactionGoods);
+        for (com.jack.recycle.model.TransactionGoods tr : transactionGoodsList) {
             String goodsName = goodsService.getGoods(tr.getGoodsId()).getGoodsName();
             tr.setGoodsId(goodsName);
         }
--- src/main/java/com/jack/recycle/model/VO/TransactionAndGoods.java
+++ src/main/java/com/jack/recycle/model/VO/TransactionAndGoods.java
@@ -1,13 +1,15 @@
 package com.jack.recycle.model.VO;
 
-import com.jack.recycle.model.Transaction;
-import com.jack.recycle.model.TransactionGoods;
+import com.jack.recycle.model.PO.TransactionGoods;
+import com.jack.recycle.model.Reservation;
 import lombok.Data;
 
 import java.util.List;
 
 @Data
 public class TransactionAndGoods {
-    private Transaction transaction;
+    private Reservation reservation;
     private List<TransactionGoods> transactionGoodsList;
+    private Double allMoney;
+    private String payFlag;
 }
