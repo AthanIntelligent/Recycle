a75afdf307397eaea6bc7879e2d65655e23a373a
aiden <1794335641@qq.com>
2022-05-18 15:46:10 +0800

    厂商管理

--- src/main/java/com/jack/recycle/controller/ManufacturersController.java
+++ src/main/java/com/jack/recycle/controller/ManufacturersController.java
@@ -7,8 +7,14 @@ import org.apache.catalina.connector.Response;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
 @CrossOrigin(origins = "*",maxAge = 3600)
 @RestController
+@RequestMapping("/facture")
 public class ManufacturersController {
     @Autowired
     ManufacturersService manufacturersService;
@@ -32,4 +38,5 @@ public class ManufacturersController {
     public Result dirManufacturers(@RequestBody Manufacturers manufacturers){
         return new Result(Response.SC_OK,"OK",manufacturersService.dirManufacturer(manufacturers));
     }
+
 }
--- src/main/java/com/jack/recycle/service/impl/ManufacturersServiceImpl.java
+++ src/main/java/com/jack/recycle/service/impl/ManufacturersServiceImpl.java
@@ -6,13 +6,19 @@ import com.jack.recycle.service.ManufacturersService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
+import java.util.UUID;
+
 @Service
 public class ManufacturersServiceImpl implements ManufacturersService {
     @Autowired
     ManufacturersDao manufacturersDao;
     @Override
     public int addManufacturer(Manufacturers manufacturer) {
+        manufacturer.setUuid(UUID.randomUUID().toString().trim());
+        manufacturer.setCreateTime(new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date()));
         return manufacturersDao.insert(manufacturer);
     }
 
